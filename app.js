2ce86e6e14e7c68b4a95a229f129dc810c1b6d07
AdnanElg <elghalbzouriadnan@gmail.com>
2022-05-30 09:10:27 +0200

    projet portfolio ajout de sass plus javascrit et animation

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// Animation typewriter js :
+
+const txtAnim1 = document.querySelector('h1');
+
+new Typewriter(txtAnim1, {
+    deleteSpeed: 100,
+})
+
+.changeDelay(100)
+.typeString('VOUS RECHERCHEZ UN <br> DÉVELOPPEUR WEB EN ALTERNANCE ?')
+.start()
+
+
+
+// Navbar button :
+
+
+
